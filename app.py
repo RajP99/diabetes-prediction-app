4a2e1a94ef102c5ca6972c283462378a311bb8fc
Raj Parikh <38576505+RajP99@users.noreply.github.com>
2021-11-27 22:44:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def predict():
 		# temp=list(json[0].values())
 		temp = [148, 72, 0, 33.6, 50]
 		dest = 'models\pkl_objects'
-    		rf = pickle.load(open(os.path.join(dest, 'rf_baseline.pkl'), 'rb'))
+    		rf = pickle.load(open('rf_baseline.pkl', 'rb'))
 		prediction = rf.predict_proba([input_values])
 		print("Prediction: ", prediction)        
 		return jsonify({'prediction': str(prediction[0])})
